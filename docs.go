66f64f7cdf2a2501f585b83258dd845f13cb8ae4
Alex Nichol <unixpickle@gmail.com>
2016-04-22 14:48:17 +0000

    added .go file to root directory

---
+++ docs.go
@@ -0,0 +1,7 @@
+// This is a suite of tools to manipulate
+// the website kahoot.it.
+//
+// It consists of several command-line
+// tools and a Go package for other Go
+// programs to interact with kahoot.it.
+package kahoothack
