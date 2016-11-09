f77408b894361af231ab0cd01ebbf8928aee5351
Alex Nichol <unixpickle@gmail.com>
2016-11-09 17:22:35 -0500

    Deal with silly arithmetic challenges.
    Works as of Nov 9, 2016. It seems that kahoot has become aware of some shenanigans, for they have now taken an active step towards obscuring their web API (albeit a rather mediocre one). I am not sure why they thought this would work--haven't they learned from AIM?

--- kahoot/sessions.go
+++ kahoot/sessions.go
@@ -1,16 +1,62 @@
 package kahoot
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strconv"
 )
 
 func gameSessionToken(gamePin int) (string, error) {
 	resp, err := http.Get("https://kahoot.it/reserve/session/" + strconv.Itoa(gamePin))
+	if resp != nil {
+		defer resp.Body.Close()
+	}
 	if err != nil {
 		return "", err
 	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
 	token := resp.Header.Get("X-Kahoot-Session-Token")
-	resp.Body.Close()
-	return token, nil
+	var bodyObj struct {
+		Challenge string `json:"challenge"`
+	}
+	if err := json.Unmarshal(body, &bodyObj); err != nil {
+		return "", fmt.Errorf("parse session challenge: %s", err)
+	}
+
+	return decipherToken(token, bodyObj.Challenge)
+}
+
+func decipherToken(xToken, challenge string) (string, error) {
+	r := bytes.NewReader([]byte(xToken))
+	base64Dec := base64.NewDecoder(base64.StdEncoding, r)
+	rawToken, err := ioutil.ReadAll(base64Dec)
+	if err != nil {
+		return "", fmt.Errorf("parse session token: %s", err)
+	}
+
+	challengeExpr := regexp.MustCompile("^\\(([0-9]*) \\+ ([0-9]*)\\) \\* ([0-9]*)$")
+	match := challengeExpr.FindStringSubmatch(challenge)
+	if match == nil {
+		return "", fmt.Errorf("unsupported challenge: %s", challenge)
+	}
+
+	num1, _ := strconv.Atoi(match[1])
+	num2, _ := strconv.Atoi(match[2])
+	num3, _ := strconv.Atoi(match[3])
+	mask := []byte(strconv.Itoa((num1 + num2) * num3))
+
+	for i := range rawToken {
+		rawToken[i] ^= mask[i%len(mask)]
+	}
+
+	return string(rawToken), nil
 }
