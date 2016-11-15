015404bd870c44c21d85200ebdce3bdfe2887da6
Alex Nichol <unixpickle@gmail.com>
2016-11-15 18:22:48 -0500

    fallback on session token brute-force
    This actually works extremely well, and should serve as a lesson: never try to roll your own security, kids.

--- kahoot/sessions.go
+++ kahoot/sessions.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -11,7 +12,21 @@ import (
 	"strconv"
 )
 
+var bruteForceErr = errors.New("not exactly one possible mask")
+
+const tokenAttempts = 20
+
 func gameSessionToken(gamePin int) (string, error) {
+	for i := 0; i < tokenAttempts; i++ {
+		token, err := attemptGameSessionToken(gamePin)
+		if err != bruteForceErr {
+			return token, err
+		}
+	}
+	return "", errors.New("could not defeat session challenge")
+}
+
+func attemptGameSessionToken(gamePin int) (string, error) {
 	resp, err := http.Get("https://kahoot.it/reserve/session/" + strconv.Itoa(gamePin))
 	if resp != nil {
 		defer resp.Body.Close()
@@ -45,7 +60,10 @@ func decipherToken(xToken, challenge string) (string, error) {
 
 	maskNum, err := computeChallenge(challenge)
 	if err != nil {
-		return "", err
+		maskNum, err = bruteForceChallenge(rawToken)
+		if err != nil {
+			return "", err
+		}
 	}
 	mask := []byte(strconv.Itoa(maskNum))
 
@@ -74,5 +92,46 @@ func computeChallenge(ch string) (int, error) {
 		return num1 * (num2 + num3), nil
 	}
 	return 0, fmt.Errorf("unsupported challenge: %s", ch)
+}
 
+func bruteForceChallenge(rawToken []byte) (int, error) {
+	var possibilities [][]byte
+LengthLoop:
+	for n := 1; n < 9; n++ {
+		possible := make([]byte, n)
+		for i := range possible {
+			possible[i] = possibleMaskByte(rawToken, n, i)
+			if possible[i] == 0 {
+				continue LengthLoop
+			}
+		}
+		possibilities = append(possibilities, possible)
+	}
+	if len(possibilities) != 1 {
+		return 0, bruteForceErr
+	}
+	return strconv.Atoi(string(possibilities[0]))
+}
+
+func possibleMaskByte(rawToken []byte, chLen, byteIdx int) byte {
+	start := 0
+	if byteIdx == 0 {
+		start = 1
+	}
+	possibs := []byte{}
+PossibilityLoop:
+	for b := start; b <= 9; b++ {
+		numChar := byte(b) + '0'
+		for i := byteIdx; i < len(rawToken); i += chLen {
+			masked := rawToken[i] ^ numChar
+			if !((masked >= 'a' && masked <= 'f') || (masked >= '0' && masked <= '9')) {
+				continue PossibilityLoop
+			}
+		}
+		possibs = append(possibs, numChar)
+	}
+	if len(possibs) != 1 {
+		return 0
+	}
+	return possibs[0]
 }
