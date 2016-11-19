7ee12a65f8c5b77c9d73fbc895c786576b924608
Alex Nichol <unixpickle@gmail.com>
2016-11-19 18:07:24 -0500

    created unit test for session token acquisition

---
+++ kahoot/sessions_test.go
@@ -0,0 +1,54 @@
+package kahoot
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"testing"
+
+	"github.com/gorilla/websocket"
+)
+
+func TestGameSessionToken(t *testing.T) {
+	resp, err := http.Post("https://play.kahoot.it/reserve/session/", "text/plain",
+		bytes.NewReader(nil))
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		t.Fatal("reserve session:", err)
+	}
+	pin, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("read session pin:", err)
+	}
+
+	pinNum, err := strconv.Atoi(string(pin))
+	if err != nil {
+		t.Fatal("invalid pin:", string(pin))
+	}
+	token, err := gameSessionToken(pinNum)
+	if err != nil {
+		t.Fatal("get token:", err)
+	}
+
+	// The server doesn't let us establish a WebSocket unless
+	// we present a valid session token.
+	conn, err := net.Dial("tcp", "kahoot.it:443")
+	if err != nil {
+		t.Fatal("website out of reach:", err)
+	}
+
+	url, _ := url.Parse("wss://kahoot.it/cometd/" + string(pin) + "/" + token)
+	reqHeader := http.Header{}
+	reqHeader.Set("Origin", "https://kahoot.it")
+	reqHeader.Set("Cookie", "no.mobitroll.session="+string(pin))
+	ws, _, err := websocket.NewClient(conn, url, reqHeader, 100, 100)
+	if err != nil {
+		t.Fatal("establish WebSocket:", err)
+	}
+	ws.Close()
+}
