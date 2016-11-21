108a5f8551adb71d3eb1bb7a698e0b04517431e3
Alex Nichol <unixpickle@gmail.com>
2016-11-21 18:06:47 -0500

    more efficient profanity sanitization
    Now, fewer letters are represented in unicode to allow for longer nicknames (since the nickname lengths are restricted by byte count or something).

--- kahoot-profane/main.go
+++ kahoot-profane/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"strconv"
@@ -60,7 +63,11 @@ func nicknames() []string {
 		fmt.Fprintln(os.Stderr, "invalid count:", os.Args[3])
 		os.Exit(1)
 	}
-	base := boldify(os.Args[2])
+	base, err := sanitizeName(os.Args[2])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to sanitize nickname:", err)
+		os.Exit(1)
+	}
 	res := make([]string, count)
 	for x := 0; x < count; x++ {
 		res[x] = base + strconv.Itoa(x+1)
@@ -68,9 +75,36 @@ func nicknames() []string {
 	return res
 }
 
-func boldify(name string) string {
+func sanitizeName(name string) (string, error) {
+	for interval := 16; interval > 1; interval-- {
+		attempt := boldify(name, interval)
+		testURL := "https://play.kahoot.it/profanity/profanities/nickname/" +
+			url.QueryEscape(attempt)
+		resp, err := http.Get(testURL)
+		if resp != nil {
+			defer resp.Body.Close()
+		}
+		if err != nil {
+			return "", err
+		}
+		contents, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		if string(contents) == "[]" {
+			return attempt, nil
+		}
+	}
+	return boldify(name, 1), nil
+}
+
+func boldify(name string, interval int) string {
 	var res []rune
-	for _, x := range name {
+	for i, x := range name {
+		if i%interval != 0 {
+			res = append(res, x)
+			continue
+		}
 		switch true {
 		case x >= 'a' && x <= 'z':
 			res = append(res, 120302+x-'a')
