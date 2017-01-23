f767d9a80ce590c81f1162ad3b090c8a6d1992d5
Noxturnix <folkgm@hotmail.com>
2017-01-23 19:51:54 +0700

    The Installer

---
+++ a.sh
@@ -0,0 +1,23 @@
+apt update
+apt install tmux
+apt install git
+apt install golang
+echo "export GOPATH=/data/data/com.termux/files/usr" > tmp1
+chmod +x tmp1
+sleep 1
+source tmp1
+go get github.com/unixpickle/kahoot-hack
+go get github.com/gorilla/websocket
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-crash/main.go ~/crash.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-flood/main.go ~/flood.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-html/main.go ~/html.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-play/main.go ~/play.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-profane/main.go ~/profane.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-rand/main.go ~/rand.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-xss/main.go ~/xss.go
+mkdir ../usr/etc/profile.d
+echo "export GOPATH=/data/data/com.termux/files/usr" > ../usr/etc/profile.d/start.sh
+chmod +x ../usr/etc/profile.d/start.sh
+rm tmp1
+rm a.sh
+echo "Installation Successful"
