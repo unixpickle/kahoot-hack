ef50d9fd8ca7f6d43c73187392f08e3052363e0d
Noxturnix <folkgm@hotmail.com>
2017-01-23 12:48:55 +0000

    Delete docs.go

--- docs.go
+++
@@ -1,7 +0,0 @@
-// This is a suite of tools to manipulate
-// the website kahoot.it.
-//
-// It consists of several command-line
-// tools and a Go package for other Go
-// programs to interact with kahoot.it.
-package kahoothack
