619f3d4a631c2ea394dd756ac50f11cb8c20b620
Noxturnix <folkgm@hotmail.com>
2017-02-24 10:48:14 +0700

    Updated

--- a.sh
+++ a.sh
@@ -2,24 +2,25 @@ apt update
 apt install tmux
 apt install git
 apt install golang
-echo "export GOPATH=/data/data/com.termux/files/usr" > tmp1
-chmod +x tmp1
+echo "export GOPATH=/data/data/com.termux/files/usr" > and_kh_tmp1
+chmod +x and_kh_tmp1
 sleep 1
-source tmp1
+source and_kh_tmp1
 echo "Downloading kahoot-hack... Please wait"
 go get github.com/unixpickle/kahoot-hack
 echo "Downloading websocket... Please wait"
 go get github.com/gorilla/websocket
-mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-crash/main.go ~/crash.go
-mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-flood/main.go ~/flood.go
-mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-html/main.go ~/html.go
-mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-play/main.go ~/play.go
-mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-profane/main.go ~/profane.go
-mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-rand/main.go ~/rand.go
-mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-xss/main.go ~/xss.go
+mkdir ~/kahoot
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-crash/main.go ~/kahoot/crash.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-flood/main.go ~/kahoot/flood.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-html/main.go ~/kahoot/html.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-play/main.go ~/kahoot/play.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-profane/main.go ~/kahoot/profane.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-rand/main.go ~/kahoot/rand.go
+mv ../usr/src/github.com/unixpickle/kahoot-hack/kahoot-xss/main.go ~/kahoot/xss.go
 mkdir ../usr/etc/profile.d
-echo "export GOPATH=/data/data/com.termux/files/usr" > ../usr/etc/profile.d/start.sh
-chmod +x ../usr/etc/profile.d/start.sh
-rm tmp1
+echo "export GOPATH=/data/data/com.termux/files/usr" >> ../usr/etc/profile.d/kahoot-hack-config.sh
+chmod +x ../usr/etc/profile.d/kahoot-hack-config.sh
+rm and_kh_tmp1
 rm a.sh
 echo "Installation Successful. Please restart the app by typing 'exit' and open the app again."
