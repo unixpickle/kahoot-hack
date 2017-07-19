3d261e8265d28cc0b52bb944e7a28be7e5bc7f2d
Peter Stenger <peter.a.stenger@gmail.com>
2017-07-19 17:50:39 -0400

    yay godoc

--- kahoot/info.go
+++ kahoot/info.go
@@ -8,41 +8,49 @@ import (
 	"net/http"
 )
 
-type ChoiceStruct struct {
+// QuizChoice represents a possible answer for a QuizQuestion.
+type QuizChoice struct {
 	Answer  string `json:"answer"`
 	Correct bool   `json:"correct"`
 }
-type VideoStruct struct {
+
+// QuizVideo is an optional video for a QuizQuestion.
+type QuizVideo struct {
 	FullUrl   string  `json:"fullUrl"`
 	Id        string  `json:"id"`
 	StartTime float64 `json:"startTime"`
 	EndTime   float64 `json:"endTime"`
 	Service   string  `json:"service"`
 }
-type QuestionStruct struct {
-	NumberOfAnswers int            `json:"numberOfAnswers"`
-	Image           string         `json:"image"`
-	Video           VideoStruct    `json:"video"`
-	Question        string         `json:"question"`
-	QuestionFormat  int            `json:"questionFormat"`
-	Time            int            `json:"time"`
-	Points          bool           `json:"points"`
-	Choices         []ChoiceStruct `json:"choices"`
-	Resources       string         `json:"resources"`
-	Type            string         `json:"type"`
+
+// QuizQuestion is a question in a quiz.
+type QuizQuestion struct {
+	NumberOfAnswers int          `json:"numberOfAnswers"`
+	Image           string       `json:"image"`
+	Video           QuizVideo    `json:"video"`
+	Question        string       `json:"question"`
+	QuestionFormat  int          `json:"questionFormat"`
+	Time            int          `json:"time"`
+	Points          bool         `json:"points"`
+	Choices         []QuizChoice `json:"choices"`
+	Resources       string       `json:"resources"`
+	Type            string       `json:"type"`
 }
-type MetadataStruct struct {
-	Resolution string           `json:"resolution"`
-	Moderation ModerationStruct `json:"moderation"`
+
+// QuizMetadata stores metadata about a quiz.
+type QuizMetadata struct {
+	Resolution string         `json:"resolution"`
+	Moderation QuizModeration `json:"moderation"`
 }
-type ModerationStruct struct {
+
+// QuizModeration stores moderator information for a quiz.
+type QuizModeration struct {
 	FlaggedTimestamp    float64 `json:"flaggedTimestamp"`
 	TimestampResolution float64 `json:"timestampResolution"`
 	Resolution          string  `json:"resolution"`
 }
 
-//############################//
-type UserStruct struct {
+type userToken struct {
 	Email        string            `json:"email"`
 	PublicAccess bool              `json:"public_access"`
 	PrimaryUsage string            `json:"primary_usage"`
@@ -57,35 +65,38 @@ type UserStruct struct {
 	Username     string            `json:"username"`
 	Birthday     []int             `json:"birthday"`
 }
-type Token struct {
+
+type token struct {
 	AccessToken        string            `json:"access_token"`
 	Expires            int               `json:"expires"`
-	User               UserStruct        `json:"user"`
+	User               userToken         `json:"user"`
 	Roles              []string          `json:"roles"`
 	CountryCode        string            `json:"countryCode"`
 	CampaignAttributes map[string]string `json:"campaignAttributes"`
 }
-type KahootQuiz struct {
-	Uuid                string           `json:"uuid"`
-	QuizType            string           `json:"quizType"`
-	Cover               string           `json:"cover"`
-	Modified            int              `json:"modified"`
-	Creator             string           `json:"creator"`
-	Audience            string           `json:"audience"`
-	Title               string           `json:"title"`
-	Description         string           `json:"description"`
-	Type                string           `json:"type"`
-	Created             int              `json:"created"`
-	Language            string           `json:"language"`
-	CreatorPrimaryUsage string           `json:"creator_primary_usage"`
-	Questions           []QuestionStruct `json:"questions"`
-	//Type                string           `json:"type"`
-	Image           string         `json:"image"`
-	Video           VideoStruct    `json:"video"`
-	Metadata        MetadataStruct `json:"metadata"`
-	Resources       string         `json:"resources"`
-	CreatorUsername string         `json:"creator_username"`
-	Visibility      int            `json:"visibility"`
+
+// QuizInfo stores information about a quiz, including
+// the correct answers.
+type QuizInfo struct {
+	Uuid                string         `json:"uuid"`
+	QuizType            string         `json:"quizType"`
+	Cover               string         `json:"cover"`
+	Modified            int            `json:"modified"`
+	Creator             string         `json:"creator"`
+	Audience            string         `json:"audience"`
+	Title               string         `json:"title"`
+	Description         string         `json:"description"`
+	Type                string         `json:"type"`
+	Created             int            `json:"created"`
+	Language            string         `json:"language"`
+	CreatorPrimaryUsage string         `json:"creator_primary_usage"`
+	Questions           []QuizQuestion `json:"questions"`
+	Image               string         `json:"image"`
+	Video               QuizVideo      `json:"video"`
+	Metadata            QuizMetadata   `json:"metadata"`
+	Resources           string         `json:"resources"`
+	CreatorUsername     string         `json:"creator_username"`
+	Visibility          int            `json:"visibility"`
 }
 
 // AccessToken returns an access token from the
