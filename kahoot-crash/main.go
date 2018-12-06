0d9795a0827ada49ca3b483c688838f5bdfce117
HALOHAWK5 <45667485+HALOHAWK5@users.noreply.github.com>
2018-12-06 11:20:55 -0600

    Update main.go

--- kahoot-crash/main.go
+++ kahoot-crash/main.go
@@ -57,7 +57,7 @@ func main() {
 	msg := kahoot.Message{
 		"data": kahoot.Message{
 			"type":    "message",
-			"gameid":  gamePin,
+			"gameid":  727728,
 			"host":    "kahoot.it",
 			"content": string(encodedContent),
 			"id":      6,
