dbdeee8c16d799dbff25d5bc305794030ab20456
tannerb1234 <57768860+tannerb1234@users.noreply.github.com>
2019-11-14 12:05:39 -0600

    Update main.go

--- kahoot-crash/main.go
+++ kahoot-crash/main.go
@@ -57,7 +57,7 @@ func main() {
 	msg := kahoot.Message{
 		"data": kahoot.Message{
 			"type":    "message",
-			"gameid":  gamePin,
+			"gameid":  184528,
 			"host":    "kahoot.it",
 			"content": string(encodedContent),
 			"id":      6,
